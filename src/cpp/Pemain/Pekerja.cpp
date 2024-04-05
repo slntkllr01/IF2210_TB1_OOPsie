80f453536a8dc51156d389a1e8b6dce4ee5dde1a
novelxv <novelyaputriramadhani11@gmail.com>
2024-04-05 12:50:03 +0700

    feat(Pekerja): add hitungPajak

--- src/cpp/Pemain/Pekerja.cpp
+++ src/cpp/Pemain/Pekerja.cpp
@@ -13,5 +13,23 @@ int Pekerja::getKKP(int kekayaan){
 
 /* Menghitung kekayaan pemain */
 int Pekerja::hitungKekayaan(){
-    return uang;
+    // Kekayaan dari inventory
+    return 0;
+}
+
+/* Menghitung pajak yang harus dibayar oleh pemain */
+int Pekerja::hitungPajak(int KKP){
+    if (KKP <= 0){
+        return 0;
+    } else if (KKP <= 6){
+        return 0.05 * KKP;
+    } else if (6 < KKP && KKP <= 25){
+        return 0.15 * KKP;
+    } else if (25 < KKP && KKP <= 50){
+        return 0.25 * KKP;
+    } else if (50 < KKP && KKP <= 500){
+        return 0.30 * KKP;
+    } else{
+        return 0.35 * KKP;
+    }
 }
