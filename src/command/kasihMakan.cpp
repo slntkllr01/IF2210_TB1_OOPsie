054d82fc9c65f0ec86fcd5b3c844b4714ee14d4b
dhiabziz <dhiditabdi@gmail.com>
2024-04-15 22:12:04 +0700

    Finalized kasihMakan

--- src/command/kasihMakan.cpp
+++ src/command/kasihMakan.cpp
@@ -9,10 +9,12 @@ void kasihMakan(Pemain* p){
     if (p->getPeran() !="Peternak"){
         throw InvalidRole();
     }
+
     Peternak* peternak = dynamic_cast<Peternak*>(p);
     if (peternak->getPeternakan().isEmpty()){
         throw PeternakanEmpty();
     }
+
     cout << "Pilih hewan yang ingin diberi makan" << endl;
     peternak->getPeternakan().CetakPeternakan();
     string slot_kandang;
@@ -28,11 +30,47 @@ void kasihMakan(Pemain* p){
         else{
             cout << "Isi petak tidak valid/kosong, silakan ulangi masukan." << endl;
         }
+    
     cout << "Pilih pangan yang akan diberikan: " << endl;
-    p->getInventory().CetakPenyimpanan();
-    //implement sini
-    //validasi petak, bisa dimakan sm hewan itu ga CanEat(Produk*)
-    //lakukan kasih makan set berat badan hewan
-    cout << peternak->getPeternakan().value(slot_kandang)->getName() << " sudah diberi makan dan beratnya menjadi " << /*berat hewan*/  endl;
-}
+
+    peternak->getInventory().CetakPenyimpanan();
+    if (peternak->getInventory().isEmpty()){
+        throw InventoryEmpty();
+    }
+
+    if (!peternak->getInventory().isThereMakanan(peternak->getPeternakan().value(slot_kandang)->get_type())){
+        throw ThereIsntMakananHewan();
+    }
+
+    string slot_inventory;
+    while (true){
+        cout << "Stok: ";
+        cin >> slot_inventory;
+        // cek slot kandang ada isi ga
+        if(peternak->getInventory().isPresent(slot_inventory) && peternak->getInventory().isLokasiValid(slot_inventory)){
+            //Sekarang validasi apakah yang dipilih itu produk
+            if(peternak->getInventory().value(slot_inventory)->getItemType() == "Produk"){
+                Produk* pr = dynamic_cast<Produk*>(peternak->getInventory().value(slot_inventory));
+                //Validasi apakah produk yang dipilih bisa dimakan
+                if(peternak->getPeternakan().value(slot_kandang)->CanEat(pr)){
+                    cout << endl;
+                    break;
+                }
+                else{
+                    cout<<"Isi petak tidak bisa dimakan oleh hewan anda. " << endl;
+                }
+            }
+            else{
+                cout<< "Isi petak bukan merupakan produk hewan/tumbuhan." << endl;
+            }
+        }
+        else{
+            cout << "Isi petak tidak valid/kosong, silakan ulangi masukan." << endl;
+        }
+    }
+
+    Produk* p = dynamic_cast<Produk*>(peternak->getInventory().value(slot_inventory));
+    peternak->beriMakanHewan(peternak->getPeternakan().value(slot_kandang), p);
+    
+    cout << peternak->getPeternakan().value(slot_kandang)->getName() << " sudah diberi makan dan beratnya menjadi " <<peternak->getPeternakan().value(slot_kandang)->get_actualweight()<<endl;}
 }
