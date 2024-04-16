b40c9bb68a186154874b2cd09fa57524d7e5f3a4
novelxv <novelyaputriramadhani11@gmail.com>
2024-04-16 20:49:11 +0700

    fix(Inventory): isThereMakananHewan

--- src/cpp/Grid/Inventory.cpp
+++ src/cpp/Grid/Inventory.cpp
@@ -234,9 +234,9 @@ string Inventory::CekJenis(string lokasi){
 }bool Inventory::isThereMakanan(string tipeHewan){
     bool thereAre = false;
     if(!this->isEmpty()){
-        for(auto it = this->getElMap().begin(); it != this->getElMap().end(); ++it){
-            if(it->second->getItemType() == "Produk"){
-                Produk* p = dynamic_cast<Produk*>(it->second);
+        for (const auto &it:this->getElMap()){
+            if(it.second->getItemType() == "Produk"){
+                Produk* p = dynamic_cast<Produk*>(it.second);
                 string tipeProduk = p->getType();
                 string checker;
                 if(tipeHewan == "HERBIVORE" && tipeProduk.find("PLANT")!= string::npos){
@@ -250,6 +250,22 @@ string Inventory::CekJenis(string lokasi){
                 }
             }
         }
+        // for(auto it = this->getElMap().begin(); it != this->getElMap().end(); ++it){
+        //     if(it->second->getItemType() == "Produk"){
+        //         Produk* p = dynamic_cast<Produk*>(it->second);
+        //         string tipeProduk = p->getType();
+        //         string checker;
+        //         if(tipeHewan == "HERBIVORE" && tipeProduk.find("PLANT")!= string::npos){
+        //             thereAre = true;
+        //         }
+        //         else if(tipeHewan == "CARNIVORE" && tipeProduk.find("ANIMAL") != string::npos){
+        //             thereAre = true;
+        //         }
+        //         else{
+        //             thereAre = true;
+        //         }
+        //     }
+        // }
     }
     return thereAre;
 }
